d08dfb641286f0c0fcd2f2fd889ec46c50464f5f
ankitborad93 <ankit.borad93@gmail.com>
2025-04-21 13:30:22 +0530

    add address Malaysia

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -236,6 +236,55 @@
                      </div>
                   </div>
                </div>
+               <div class="bt_bb_port new-contact-section">
+                  <div class="bt_bb_cell">
+                     <div class="bt_bb_cell_inner">
+                        <div class="bt_bb_row_wrapper">
+                           <div class="bt_bb_row">
+                              <div class="bt_bb_column col-12 col-xxl-4 col-xl-4 bt_bb_vertical_align_bottom bt_bb_align_center bt_bb_padding_normal bt_bb_animation_fade_in animate animated">
+                                 <div class="location-details">
+                                    <div class="bt_bb_icon bt_bb_color_scheme_3 bt_bb_style_borderless bt_bb_size_small bt_bb_shape_circle bt_bb_target_self bt_bb_align_inherit bt_bb_text_color_scheme_5 bt_bb_position_top" style="--primary-color:#09449f; --secondary-color:#000000;; --text-primary-color:#000000; --text-secondary-color:#09449f;" data-bt-override-class="{}">
+                                       <span data-ico-dripicons="" class="bt_bb_icon_holder">
+                                          <span>
+                                                UK <br>
+                                                Mr. Shvetang<br>
+                                                65 Cornelius St, Coventry, CV35FJ, United Kingdom
+                                          </span>
+                                       </span>
+                                    </div>
+                                 </div>
+                              </div>
+                              <div class="bt_bb_column col-12 col-xxl-4 col-xl-4 bt_bb_vertical_align_bottom bt_bb_align_center bt_bb_padding_normal bt_bb_animation_fade_in animate animated">
+                                 <div class="location-details">
+                                    <div class="bt_bb_icon bt_bb_color_scheme_3 bt_bb_style_borderless bt_bb_size_small bt_bb_shape_circle bt_bb_target_self bt_bb_align_inherit bt_bb_text_color_scheme_5 bt_bb_position_top" style="--primary-color:#09449f; --secondary-color:#000000;; --text-primary-color:#000000; --text-secondary-color:#09449f;" data-bt-override-class="{}">
+                                       <span data-ico-dripicons="" class="bt_bb_icon_holder">
+                                          <span>
+                                                Israel<br>
+                                                Ms. Batia<br>
+                                                No. 4 Heletz St.Haifa, Israel
+                                          </span>
+                                       </span>
+                                    </div>
+                                 </div>
+                              </div>
+                              <div class="bt_bb_column col-12 col-xxl-4 col-xl-4 bt_bb_vertical_align_bottom bt_bb_align_center bt_bb_padding_normal bt_bb_animation_fade_in animate animated">
+                                <div class="location-details">
+                                   <div class="bt_bb_icon bt_bb_color_scheme_3 bt_bb_style_borderless bt_bb_size_small bt_bb_shape_circle bt_bb_target_self bt_bb_align_inherit bt_bb_text_color_scheme_5 bt_bb_position_top" style="--primary-color:#09449f; --secondary-color:#000000;; --text-primary-color:#000000; --text-secondary-color:#09449f;" data-bt-override-class="{}">
+                                      <span data-ico-dripicons="" class="bt_bb_icon_holder">
+                                         <span>
+                                              Malaysia<br>
+                                              Mr. David<br>
+                                              T3-23-20, Menara 3, 296, Jln Ampang, Kampung Berembang, 55000 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur
+                                         </span>
+                                      </span>
+                                   </div>
+                                </div>
+                             </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
             </section>
             <!-- new section -->
 			<section id="bt_bb_section647db95b6306e" data-parallax="0.7" data-parallax-offset="-100" class="bt_bb_section bt_bb_color_scheme_2 bt_bb_layout_boxed_1200 bt_bb_vertical_align_top bt_bb_background_image bt_bb_parallax bt_bb_top_spacing_medium bt_bb_bottom_spacing_medium" data-bt-override-class="null">
--- resources/views/front/layout/footer.blade.php
+++ resources/views/front/layout/footer.blade.php
@@ -181,6 +181,47 @@
                               </div>
                            </div>
                         </div>
+                        <div class="location-details bt_bb_row">
+                           <div class="bt_bb_column col-12 col-xxl-4 col-xl-4 bt_bb_vertical_align_bottom bt_bb_align_center bt_bb_padding_normal bt_bb_animation_fade_in animate animated">
+                              <div class="location-details">
+                                 <div class="bt_bb_icon bt_bb_color_scheme_3 bt_bb_style_borderless bt_bb_size_small bt_bb_shape_circle bt_bb_target_self bt_bb_align_inherit bt_bb_text_color_scheme_5 bt_bb_position_top" style="--primary-color:#09449f; --secondary-color:#000000;; --text-primary-color:#000000; --text-secondary-color:#09449f;" data-bt-override-class="{}">
+                                    <span data-ico-dripicons="" class="bt_bb_icon_holder">
+                                       <span>
+                                            UK <br>
+                                            Mr. Shvetang<br>
+                                            65 Cornelius St, Coventry, CV35FJ, United Kingdom
+                                       </span>
+                                    </span>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="bt_bb_column col-12 col-xxl-4 col-xl-4 bt_bb_vertical_align_bottom bt_bb_align_center bt_bb_padding_normal bt_bb_animation_fade_in animate animated">
+                              <div class="location-details">
+                                 <div class="bt_bb_icon bt_bb_color_scheme_3 bt_bb_style_borderless bt_bb_size_small bt_bb_shape_circle bt_bb_target_self bt_bb_align_inherit bt_bb_text_color_scheme_5 bt_bb_position_top" style="--primary-color:#09449f; --secondary-color:#000000;; --text-primary-color:#000000; --text-secondary-color:#09449f;" data-bt-override-class="{}">
+                                    <span data-ico-dripicons="" class="bt_bb_icon_holder">
+                                       <span>
+                                            Israel<br>
+                                            Ms. Batia<br>
+                                            No. 4 Heletz St.Haifa, Israel
+                                       </span>
+                                    </span>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="bt_bb_column col-12 col-xxl-4 col-xl-4 bt_bb_vertical_align_bottom bt_bb_align_center bt_bb_padding_normal bt_bb_animation_fade_in animate animated">
+                            <div class="location-details">
+                               <div class="bt_bb_icon bt_bb_color_scheme_3 bt_bb_style_borderless bt_bb_size_small bt_bb_shape_circle bt_bb_target_self bt_bb_align_inherit bt_bb_text_color_scheme_5 bt_bb_position_top" style="--primary-color:#09449f; --secondary-color:#000000;; --text-primary-color:#000000; --text-secondary-color:#09449f;" data-bt-override-class="{}">
+                                  <span data-ico-dripicons="" class="bt_bb_icon_holder">
+                                     <span>
+                                          Malaysia<br>
+                                          Mr. David<br>
+                                          T3-23-20, Menara 3, 296, Jln Ampang, Kampung Berembang, 55000 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur
+                                     </span>
+                                  </span>
+                               </div>
+                            </div>
+                         </div>
+                        </div>
                      </div>
                   </div>
                   <!-- Start::Copyright Section -->
